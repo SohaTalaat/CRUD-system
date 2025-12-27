6c9fc7a16a15703212ec32cdbc66e4372bf26085
Soha <saha.20350436@science.aun.edu.eg>
2025-12-27 21:26:09 +0200

    Create migrations

--- backend/database/migrations/2025_12_25_113853_create_admins_table.php
+++ backend/database/migrations/2025_12_25_113853_create_admins_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone')->unique();
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
